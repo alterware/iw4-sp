8d6c76f501afe77f3adb118be44ea34d1ef38a77
Diavolo <edoardo.sanguineti222@gmail.com>
2023-06-01 13:10:18 +0200

    maint: add back xlabs hash, removing was bad

--- src/client/loader/binary_loader.cpp
+++ src/client/loader/binary_loader.cpp
@@ -7,6 +7,9 @@
 #define SP_HASH                                                                \
   "39E9C4FACEA4B19017BB8C2FC64E4149708927AC40C7C3DEACC25BDD25E93D32"
 
+#define SP_XLABS_HASH                                                          \
+  "05D499D77028859D4BA30C852DA85CCA5F02678B22AEA9E27D7C56973B14A0BC"
+
 namespace binary_loader {
 std::string load_base() {
   std::string data;
@@ -15,7 +18,7 @@ std::string load_base() {
   }
 
   const auto hash = utils::cryptography::sha256::compute(data, true);
-  if (hash != SP_HASH) {
+  if ((hash != SP_XLABS_HASH) && (hash != SP_HASH)) {
     throw std::runtime_error(
         "Your iw4sp.exe is incompatible with this client.");
   }
